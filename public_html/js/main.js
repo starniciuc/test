ab6ffc281851c29d2694971c7f534f17f3685062
Mihai <Mihai@Mihai.Dlink>
2015-06-04 15:53:42 +0300

    slice full site

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,5 +2,15 @@ $(document).foundation();
 
 $('.home-slider').superslides({
   pagination: true,
+  //play: 10000,
   scrollable: true
 });
+
+function goScroll(a) {
+    if (a != 0) {
+        $('html, body').animate({scrollTop: $("#" + a).offset().top}, 700);
+    } else {
+        $('html, body').animate({scrollTop: 0}, 700);
+    }
+
+}
